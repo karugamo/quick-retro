7e591e18073af0b94850d923e1405aa2f225046f
Finn Pauls <mbp@pauls.dev>
2022-05-03 11:45:00 +0200

    Add new cards

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
-const title = 'Items';
-const items = [
-  {
-    text: 'Item 1',
-  },
-  {
-    text: 'Item 2',
-  },
-  {
-    text: 'Item 3',
+import { ref } from "vue";
+import Card from "./Card.vue";
 
-  },
-]
+const title = "Items";
+const items = ref([{ text: "example" }]);
+const inputText = ref("");
+
+function onAdd(e: Event) {
+  e.preventDefault();
+  items.value.push({ text: inputText.value });
+  inputText.value = "";
+}
 </script>
 
 <template>
   <h2>{{ title }}</h2>
   <ul>
-    <li v-for="item in items">
-      {{ item.text }}
-    </li>
+    <Card v-for="item in items" :text="item.text" />
   </ul>
+  <form target="#" @submit="onAdd">
+    <input v-model="inputText" />
+  </form>
 </template>
-
-<style scoped>
-li {
-  list-style: none;
-}
-</style>
