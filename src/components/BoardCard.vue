4bc31f01745684ae35240358b3862c6226cc5619
Finn Pauls <mbp@pauls.dev>
2022-05-04 20:19:38 +0200

    Refactor HomeView to use a BoardCard

---
+++ src/components/BoardCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="board" :class="{template: isTemplate}" @click="$emit('click')">
+    <h3>{{ board.title || "Untitled Retro" }}</h3>
+    <div class="columns">
+      <div
+        class="column"
+        v-for="column in board.columns"
+        v-bind:style="{ backgroundColor: column.color }"
+      >
+        {{ column.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Template } from "../templates";
+import BoardData from "../types";
+
+const { board, isTemplate } = defineProps<{
+  board: BoardData | Template;
+  isTemplate: boolean;
+}>();
+</script>
+
+<style scoped>
+.board {
+  width: 300px;
+  height: 200px;
+  border: 2px solid rgba(47, 43, 43, 0.309);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 16px;
+  padding: 16px;
+  font-weight: bold;
+  transition: all 0.2s ease-in-out;
+}
+
+.board:hover {
+  box-shadow: 0 0 8px rgba(47, 43, 43, 0.115);
+  transform: scale(1.05);
+}
+
+.columns {
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  flex: 1;
+  width: 100%;
+}
+
+.column {
+  background-color: red;
+  border-radius: 4px;
+  color: white;
+  flex: 1;
+  padding: 8px;
+  min-width: 0;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.template {
+  border-style: dashed;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,42 +4,21 @@
       <h1>🚀 quick retro 🚀</h1>
       <h2>start a new board</h2>
       <section>
-        <div
-          class="board template"
+        <BoardCard
           v-for="template in templates"
           @click="createNewBoard(template)"
-        >
-          <h3>{{ template.title }}</h3>
-          <div class="columns">
-            <div
-              class="column"
-              v-for="column in template.columns"
-              v-bind:style="{ backgroundColor: column.color }"
-              :title="column.title"
-            >
-              {{ column.title }}
-            </div>
-          </div>
-        </div>
+          :board="template"
+          :is-template="true"
+        />
       </section>
       <h2>your boards</h2>
       <section>
-        <div
-          class="board"
-          @click="navigateToBoard(boardId as unknown as string)"
+        <BoardCard
           v-for="(board, boardId) in ownBoards"
-        >
-          <h3>{{ board.title || "Untitled Retro" }}</h3>
-          <div class="columns">
-            <div
-              class="column"
-              v-for="column in board.columns"
-              v-bind:style="{ backgroundColor: column.color }"
-            >
-              {{ column.title }}
-            </div>
-          </div>
-        </div>
+          @click="navigateToBoard(boardId as unknown as string)"
+          :board="board"
+          :is-template="false"
+        />
       </section>
     </main>
   </div>
@@ -47,6 +26,7 @@
 
 <script setup lang="ts">
 import { computed, inject } from "vue";
+import BoardCard from "../components/BoardCard.vue";
 import { addNewBoard, useBoards } from "../database";
 import { Template, templates } from "../templates";
 import BoardData from "../types";
@@ -88,50 +68,6 @@ section {
   margin-top: 32px;
 }
 
-.board {
-  width: 300px;
-  height: 200px;
-  border: 2px solid rgba(47, 43, 43, 0.309);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  user-select: none;
-  border-radius: 4px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  gap: 16px;
-  padding: 16px;
-  font-weight: bold;
-  transition: all 0.2s ease-in-out;
-}
-
-.board:hover {
-  box-shadow: 0 0 8px rgba(47, 43, 43, 0.115);
-  transform: scale(1.05);
-}
-
-.columns {
-  display: flex;
-  flex-direction: row;
-  gap: 4px;
-  flex: 1;
-  width: 100%;
-}
-
-.column {
-  background-color: red;
-  border-radius: 4px;
-  color: white;
-  flex: 1;
-  padding: 8px;
-  min-width: 0;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-}
-
 main {
   padding: 32px;
   width: 100%;
@@ -154,9 +90,5 @@ h2 {
   font-size: 28px;
   font-weight: bold;
   margin-top: 32px;
-}
-
-.template {
-  border-style: dashed;
 }
 </style>
