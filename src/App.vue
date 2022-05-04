5ee69ff62d145e92418b27ea1bbadb9a805469e5
Finn Pauls <mbp@pauls.dev>
2022-05-04 18:23:45 +0200

    Switch to hash router to work with Github Pages

--- src/App.vue
+++ src/App.vue
@@ -10,28 +10,27 @@ import BoardView from "./views/BoardView.vue";
 import HomeView from "./views/HomeView.vue";
 
 export interface Board {
-  author: string
-  title: string
+  author: string;
+  title: string;
   cardsHidden: boolean;
   columns: { [columnId: string]: any };
   loading: boolean;
   exists: boolean;
 }
 
-const boardId = ref(window.location.pathname.split("/").pop());
+const boardId = ref(window.location.hash.slice(1));
+console.log(window.location.hash);
 
 function navigateToBoard(newBoardId: string) {
-  history.pushState({}, "", `/quick-retro/${newBoardId}`);
+  history.pushState({}, "", `#${newBoardId}`);
   boardId.value = newBoardId;
 }
 
 addEventListener("popstate", () => {
-  boardId.value = window.location.pathname.split("/").pop();
+  boardId.value = window.location.hash.slice(1);
 });
-
 
 const user = useUser() as { uid: string };
 
 provide("user", user);
 </script>
-
