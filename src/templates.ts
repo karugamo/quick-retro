a109f45937c32daa368d55ac4f843ba3fdce259c
Finn Pauls <mbp@pauls.dev>
2025-03-03 08:57:57 +0000

    Add three more retro formats

--- src/templates.ts
+++ src/templates.ts
@@ -64,4 +64,67 @@ export const templates: Template[] = [
       },
     ],
   },
+  {
+    title: "The Four L's",
+    columns: [
+      {
+        title: "Liked",
+        color: "#4CAF50", // Green
+      },
+      {
+        title: "Learned",
+        color: "#2196F3", // Blue
+      },
+      {
+        title: "Lacked",
+        color: "#FF9800", // Orange
+      },
+      {
+        title: "Longed For",
+        color: "#9C27B0", // Purple
+      },
+    ],
+  },
+  {
+    title: "KALM",
+    columns: [
+      {
+        title: "Keep",
+        color: "#4CAF50", // Green
+      },
+      {
+        title: "Add",
+        color: "#2196F3", // Blue
+      },
+      {
+        title: "Less",
+        color: "#FF5722", // Deep Orange
+      },
+      {
+        title: "More",
+        color: "#673AB7", // Deep Purple
+      },
+    ],
+  },
+  {
+    title: "DAKI",
+    columns: [
+      {
+        title: "Drop",
+        color: "#F44336", // Red
+      },
+      {
+        title: "Add",
+        color: "#4CAF50", // Green
+      },
+      {
+        title: "Keep",
+        color: "#2196F3", // Blue
+      },
+      {
+        title: "Improve",
+        color: "#FFC107", // Amber
+      },
+    ],
+  },
 ];
